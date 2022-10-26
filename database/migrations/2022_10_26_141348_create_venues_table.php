57183b736018d0a92d4fa7cd76d8063b1e47b421
RuariM <48099261+RuariM@users.noreply.github.com>
2022-10-26 15:56:03 +0100

    venue and address migration and models.

---
+++ database/migrations/2022_10_26_141348_create_venues_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('venues', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('capacity');
+            $table->string('venue_type');
+            $table->time('opening_time');
+            $table->time('clossing_time');
+            $table->integer('rating')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('venues');
+    }
+};
