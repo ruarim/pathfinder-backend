09de9e0f64372cfe1c2f0644955701db50b4e548
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-11-10 11:41:53 +0000

    fixing the styling

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -4,26 +4,25 @@
         <div class="flex">
           <div class="flex flex-shrink-0 items-center">
           </div>
-          <div class="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+          <div class="-my-px ml-6 flex space-x-8">
             <x-nav-link route="dashboard">Dashboard</x-nav-link>
             <x-nav-link route="venues">Venues</x-nav-link>
             <x-nav-link route="users">Users</x-nav-link>
           </div>
         </div>
-        <div class="hidden sm:ml-6 sm:flex sm:items-center">
+        <div class="ml-6 flex items-center">
           <x-profile-dropdown>
             <x-slot name="trigger">
                 <button type="button" class="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                     <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
                 </button>
             </x-slot>
-
                 <a href="/profile" class="block px-4 py-2 text-sm text-gray-700 hover:bg-slate-500/10 transition" role="menuitem" tabindex="-1" id="user-menu-item-1">My Profile</a>
                 <a href="/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-slate-500/10 transition" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-
             </x-profile-dropdown>
           </div>
         </div>
+
       </div>
     </div>
   </nav>
