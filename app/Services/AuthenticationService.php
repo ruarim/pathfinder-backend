25b49692dd21088daaf03a8bbcc8e9298786ac9f
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-11-16 19:24:49 +0000

    configuring cors headers and returning a token on register

--- app/Services/AuthenticationService.php
+++ app/Services/AuthenticationService.php
@@ -6,10 +6,6 @@ use App\Contracts\AuthenticationServiceInterface;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
-
-use Exception;
 use Illuminate\Support\Facades\Hash;
 
 class AuthenticationService implements AuthenticationServiceInterface
@@ -23,24 +19,30 @@ class AuthenticationService implements AuthenticationServiceInterface
     public function login(LoginRequest $loginRequest)
     {
         if (auth()->attempt($loginRequest->all())) {
-           $user = auth()->user();
-           dd($user);
-           $token = $user->createToken('authToken')->accessToken->token;
-           return [
-               'user' => $user,
-               'token' =>  $token
-            ];
+            $user = auth()->user();
+            $token = $user->createToken('authToken')->accessToken->token;
+            return [
+                'user' => $user,
+                'token' =>  $token
+             ];
         }
     }
 
     public function register(RegisterRequest $registerRequest)
     {
-        return User::create([
+        $user = User::create([
             'first_name' => $registerRequest->first_name,
             'last_name' => $registerRequest->last_name,
             'username' => $registerRequest->username,
             'email' => $registerRequest->email,
             'password' => Hash::make($registerRequest->password)
         ]);
+
+        $token = $user->createToken('authToken')->accessToken->token;
+
+        return [
+            'user' => $user,
+            'token' =>  $token
+         ];
     }
 }
