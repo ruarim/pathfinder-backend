c19c1ebb9197a4657438eee539410dc6549c918b
RuariM <48099261+RuariM@users.noreply.github.com>
2022-12-03 20:57:06 +0000

    first draft

--- app/Models/Venue.php
+++ app/Models/Venue.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Address;
 use App\Models\Attribute;
+use Illuminate\Support\Facades\Http;
 
 class Venue extends Model
 {
@@ -48,11 +49,28 @@ class Venue extends Model
         return $this;
     }
 
-    public function setBeverages(array $strings): Venue
+    public function setBeverages(array $strings): Venue //change to beverages
     {
         $beverages = Beverage::fromArray($strings);
         $this->beverages()->sync($beverages->pluck("id"));
 
+        return $this;
+    }
+
+    public function setAddress(array $address_data): Venue
+    {
+        //create params from address_data
+        $address1 = $address_data['address1'];
+        $city = $address_data['city'];
+        $country = $address_data['country'];
+        $response = Http::get('https://nominatim.openstreetmap.org/search?q="13+fernbank+road,+bristol&format=json&polygon=1&addressdetails=1');
+
+        $address_data['latitude'] = $response->json()['lat'];
+        $address_data['longitude'] = $response->json()['lon'];
+
+        dd($address_data);
+        $address = new Address($address_data);
+        $this->address()->save($address);
         return $this;
     }
 }
