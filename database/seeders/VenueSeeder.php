f7d2afd063ec0f4774b04f48b92c4f6486c50317
RuariM <48099261+RuariM@users.noreply.github.com>
2023-03-14 19:36:23 +0000

    Helper -> helper

--- database/seeders/VenueSeeder.php
+++ database/seeders/VenueSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Helpers\Calculations;
+use App\helpers\Calculations;
 use App\Models\Rating;
 use App\Models\Review;
 use App\Models\User;
