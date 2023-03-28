58b08524dc2269f785b2b06f8e783840538216f0
RuariM <48099261+RuariM@users.noreply.github.com>
2023-03-28 20:16:59 +0100

    calulations hotfix

--- app/Http/Controllers/Api/PathController.php
+++ app/Http/Controllers/Api/PathController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Helpers\Calculations;
+use App\helpers\Calculations;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PathResource;
 use App\Http\Resources\RatingResource;
