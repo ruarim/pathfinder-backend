135ce274b383b93af0135ba011cb14d164fd1c31
RuariM <48099261+RuariM@users.noreply.github.com>
2023-08-10 20:40:15 +0100

    make seeder more readable, rename helpers

--- database/seeders/VenueSeeder.php
+++ database/seeders/VenueSeeder.php
@@ -5,12 +5,13 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Address;
-use App\helpers\Calculations;
+use App\Helpers\Calculations;
 use App\Models\Rating;
 use App\Models\Review;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use App\Models\Venue;
+use Database\Seeders\metadata\Foursquare;
 
 class VenueSeeder extends Seeder
 {
@@ -20,187 +21,164 @@ class VenueSeeder extends Seeder
      * @return void
      */
 
-    private $client;
-    private $seed_locations;
-    private $seed_radius;
-    private $seed_limit;
-    private $auth_token;
+    private $seedLocations;
+    private $seedRadius;
+    private $seedLimit;
+    private $foursquare;
+
 
     public function __construct()
     {
-        $this->client = new \GuzzleHttp\Client();
         $env_string = env('SEED_LOCATIONS');
-        $this->seed_locations = explode(";", $env_string);
-        $this->seed_limit = env('SEED_LIMIT');
-        $this->seed_radius = env('SEED_RADIUS');
-        $this->auth_token = env('FOURSQUARE_SECRET');
+        $this->seedLocations = explode(";", $env_string);
+        $this->seedRadius = env('SEED_RADIUS');
+        $this->seedLimit = env('SEED_LIMIT');
+        $this->foursquare = new Foursquare();
     }
 
     public function run()
     {
         echo "Seeding venues\n";
 
-        $email = 'seeder@seeder.com';
-        $user = User::firstOrCreate([
-            'username' => 'James',
-            'email' => $email,
-            'password' => env('SEEDER_PASSWORD'),
-            'avatar_url' => 'https://api.dicebear.com/5.x/thumbs/svg?backgroundColor=b6e3f4&seed=' . $email,
-        ]);
+        $user = $this->seedUser();
+        $totalSeeded = 0;
 
-        $fully_seeded = 0;
-        foreach ($this->seed_locations as $location) {
-            $lat_long = explode(",", $location);
-            $venues = $this->getVenues($lat_long[0], $lat_long[1], $this->seed_radius, $this->seed_limit)['results'];
+        foreach ($this->seedLocations as $location) {
+            $latLong = explode(",", $location);
+            echo 'Seeding location: ' . "lat: {$latLong[0]}, long: {$latLong[1]}\n";
+
+            $venues = $this->foursquare
+                ->getVenues(
+                    $latLong[0],
+                    $latLong[1],
+                    $this->seedRadius,
+                    $this->seedLimit
+                )['results'];
 
             foreach ($venues as $venue) {
-                $this->seedVenue($venue, $user->id, $fully_seeded);
+                echo "Seeding {$venue['name']}...\n";
+                $metadata = $this->foursquare->getVenueDetails($venue['fsq_id']);
+
+                $model = $this->seedVenue(
+                    $venue,
+                    $metadata,
+                    $user->id
+                );
+
+                //does model need to be passed by ref here?
+                $this->seedAddress(
+                    $venue,
+                    $model
+                );
+
+                //move build here
+                $this->seedAttributes(
+                    $metadata,
+                    $model
+                );
+
+                $this->seedImages(
+                    $metadata,
+                    $model
+                );
+
+                $this->seedReviews(
+                    $metadata,
+                    $model
+                );
+
+                $this->seedRating(
+                    $metadata,
+                    $model,
+                );
+
+                $totalSeeded++;
+                echo "Seeded {$model->name}\n";
             }
         }
 
-        echo "Seeded {$fully_seeded} venues\n";
+        echo "Seeded {$totalSeeded} venues\n";
     }
 
-
-    //move to requests file
-    private function getVenues($lat, $long, $radius, $limit)
+    private function seedVenue($venue, $metadata, $userId)
     {
-        $categories = '13018';
-        $exclude_chains = '07841cd9-3716-4bd6-8ebd-ad13070b3125';
-        $url = "https://api.foursquare.com/v3/places/search?ll={$lat},{$long}&radius={$radius}&categories={$categories}&sort=DISTANCE&limit={$limit}&exclude_chains={$exclude_chains}";
+        $venueData = $this->foursquare->buildVenue(
+            $venue,
+            $metadata,
+            $userId
+        );
+        if (!$venueData) return null;
 
-        $response = $this->client->request('GET', $url, [
-            'headers' => [
-                'Authorization' => $this->auth_token,
-                'accept' => 'application/json',
-            ],
-        ]);
+        $model = Venue::firstOrCreate($venueData);
 
-        $body = $response->getBody();
-        return json_decode($body, true);
+        //is this doing anything?
+        if (!$model) return;
+        $model->save();
+
+        return $model;
     }
 
-    private function getVenueDetails(string $id)
+    private function seedAddress($venueData, $model)
     {
-        $fields = 'photos,tel,website,description,name,features,tastes,hours,rating,tips';
-        $url = "https://api.foursquare.com/v3/places/{$id}?fields={$fields}";
+        //replace with first or create need to exlude id?
+        $addressExists = Address::where(
+            'venue_id',
+            $model->id,
+        )->first();
 
-        $response = $this->client->request('GET', $url, [
-            'headers' => [
-                'Authorization' => $this->auth_token,
-                'accept' => 'application/json',
-            ],
-        ]);
-
-        $body = $response->getBody();
-        return json_decode($body, true);
-    }
-
-    private function seedVenue($venue_data, $user_id, &$fully_seeded)
-    {
-        echo "Seeding {$venue_data['name']}...\n";
-
-        $fsq_id = $venue_data['fsq_id'];
-        $venue_details = $this->getVenueDetails($fsq_id);
-
-        $name = $venue_data["name"];
-        $venue_type = $venue_data["categories"][0]['name'];
-
-        $hours = '';
-        $hours_data = $venue_details['hours'];
-        if (key_exists('display', $hours_data)) $hours = $hours_data['display'];
-
-        $description = '';
-        if (key_exists('description', $venue_details)) $description = $venue_details['description'];
-
-        $venue_model = Venue::firstOrCreate([
-            'user_id' => $user_id,
-            "name" => $name,
-            "venue_type" => $venue_type,
-            "hours" => $hours,
-            "description" => $description,
-        ]);
-
-        if (!$venue_model) return;
-        $venue_model->save();
-
-        $this->seedAddress($venue_data, $venue_model);
-
-        if (!key_exists('tastes', $venue_details)) return;
-        $attributes = $venue_details['tastes'];
-        $this->seedAttributes($attributes, $venue_model);
-
-        $image_urls = [];
-
-        $images = $venue_details['photos'];
-        foreach ($images as $image) {
-            array_push($image_urls, $image['prefix'] . 'original' . $image['suffix']);
+        if ($addressExists)
+            $model->address()
+                ->save($addressExists);
+        else {
+            $address = $this->foursquare->buildAddress($venueData);
+            if (!$address) return null;
+            $address['venue_id'] = $model->id;
+            $model->setAddress($address);
         }
-        $venue_model->setImages($image_urls);
+    }
 
-        if (!key_exists('tips', $venue_details)) return;
-        $reviews = $venue_details['tips'];
+    private function seedAttributes($metadata, $model)
+    {
+        $attributes = $this->foursquare->buildAttributes($metadata);
+        if (!$attributes) return null;
+
+        $model->setAttributes($attributes);
+    }
+
+    private function seedImages($metadata, $model)
+    {
+        $images = $this->foursquare->buildImages($metadata);
+        if (!$images) return null;
+
+        $model->setImages($images);
+    }
+
+    private function seedReviews($metadata, $model)
+    {
+        $reviews = $this->foursquare->buildReviews($metadata);
+
+        if (!$reviews) return null;
 
         foreach ($reviews as $review) {
-            $this->seedReview($review['text'], $venue_model);
-        }
-
-        $rating = $venue_details['rating'];
-        $this->seedRating($rating, $venue_model, $user_id);
-
-        $fully_seeded++;
-        echo "Seeded {$venue_model->name}\n";
-    }
-
-    private function seedAddress($venue_data, $venue_model)
-    {
-        $address_data = $venue_data['location'];
-        $lat = $venue_data['geocodes']['main']['latitude'];
-        $long = $venue_data['geocodes']['main']['longitude'];
-
-        $address_exists = Address::where('venue_id', $venue_model->id,)
-            ->first();
-
-        if ($address_exists) $venue_model
-            ->address()
-            ->save($address_exists);
-        else {
-            $address_data = [
-                'venue_id' => $venue_model->id,
-                'address_1' => $address_data['address'],
-                'town_city' => $address_data['post_town'],
-                'postcode' => $address_data['postcode'],
-                'country' => $address_data['admin_region'],
-                'latitude' => $lat,
-                'longitude' => $long,
-            ];
-            $venue_model
-                ->setAddress($address_data);
+            $user = $this->fakeUser();
+            Review::firstOrCreate([
+                'user_id' => $user->id,
+                'venue_id' => $model->id,
+                'content' => $review['text'],
+            ]);
         }
     }
 
-    private function seedAttributes($attributes, $venue_model)
+    private function seedRating($metadata, $model)
     {
-        $venue_model->setAttributes($attributes);
-    }
+        $rating = $this->foursquare->buildRating($metadata);
+        if (!$rating) return null;
 
-    private function seedReview($review, $venue_model)
-    {
-        $user = $this->fakeUser();
-        Review::firstOrCreate([
-            'user_id' => $user->id,
-            'venue_id' => $venue_model->id,
-            'content' => $review,
-        ]);
-    }
-
-    private function seedRating($rating, $venue_model)
-    {
         $user = $this->fakeUser();
 
         Rating::firstOrCreate(
             [
-                'rateable_id' => $venue_model->id,
+                'rateable_id' => $model->id,
                 'rateable_type' => Venue::class,
                 'user_id' => $user->id,
             ],
@@ -209,10 +187,10 @@ class VenueSeeder extends Seeder
             ]
         );
 
-        $ratings = collect($venue_model->ratings);
+        $ratings = collect($model->ratings);
         $avg_rating = Calculations::calculate_average_rating($ratings);
-        $venue_model->rating = $avg_rating;
-        $venue_model->save();
+        $model->rating = $avg_rating;
+        $model->save();
     }
 
     private function fakeUser()
@@ -229,6 +207,20 @@ class VenueSeeder extends Seeder
             'avatar_url' => 'https://api.dicebear.com/5.x/thumbs/svg?backgroundColor=b6e3f4&seed=' . $email,
         ]);
 
+        return $user;
+    }
+
+    private function seedUser()
+    {
+        $email = 'seeder@seeder.com';
+        $user = User::firstOrCreate(
+            [
+                'username' => 'James',
+                'email' => $email,
+                'password' => env('SEEDER_PASSWORD'),
+                'avatar_url' => 'https://api.dicebear.com/5.x/thumbs/svg?backgroundColor=b6e3f4&seed=' . $email,
+            ]
+        );
         return $user;
     }
 }
---
+++ database/seeders/metadata/foursquare.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Database\Seeders\metadata;
+
+use Illuminate\Support\Arr;
+use Illuminate\Contracts\Container\BindingResolutionException;
+
+class Foursquare
+{
+    private $client;
+    private $auth_token;
+
+    public function __construct()
+    {
+        $this->client = new \GuzzleHttp\Client();
+        $this->auth_token = env('FOURSQUARE_SECRET');
+    }
+
+    public function getVenues(float $lat, float $long, int $radius, int $limit)
+    {
+        $categories = '13018';
+        $exclude_chains = '07841cd9-3716-4bd6-8ebd-ad13070b3125';
+        $url = "https://api.foursquare.com/v3/places/search?ll={$lat},{$long}&radius={$radius}&categories={$categories}&sort=DISTANCE&limit={$limit}&exclude_chains={$exclude_chains}";
+
+        $response =
+            $this
+            ->client
+            ->request('GET', $url, [
+                'headers' => [
+                    'Authorization' => $this->auth_token,
+                    'accept' => 'application/json',
+                ],
+            ]);
+
+        $body = $response->getBody();
+        return json_decode($body, true);
+    }
+
+    public function getVenueDetails(string $id)
+    {
+        $fields = 'photos,tel,website,description,name,features,tastes,hours,rating,tips';
+        $url = "https://api.foursquare.com/v3/places/{$id}?fields={$fields}";
+
+        $response =
+            $this
+            ->client
+            ->request('GET', $url, [
+                'headers' => [
+                    'Authorization' => $this->auth_token,
+                    'accept' => 'application/json',
+                ],
+            ]);
+
+        $body = $response->getBody();
+        return json_decode($body, true);
+    }
+
+    public function buildVenue($venueData, $metadata, int $userId)
+    {
+        try {
+            $name = Arr::get($venueData, "name");
+            $venueType = Arr::get($venueData, "categories.0.name");
+            $hours = Arr::get($metadata, 'hours.display', '');
+            $description = Arr::get($metadata, 'description', '');
+
+            $venue = [
+                'user_id' => $userId,
+                "name" => $name,
+                "venue_type" => $venueType,
+                "hours" => $hours,
+                "description" => $description,
+            ];
+
+            return $venue;
+        } catch (BindingResolutionException $e) {
+            echo 'Error building venue. Caught exception: ',  $e->getMessage(), "\n";
+            return null;
+        }
+    }
+
+    public function buildAddress($venue)
+    {
+        try {
+            $addressData = Arr::get($venue, 'location');
+            $lat = Arr::get($venue, 'geocodes.main.latitude');
+            $long = Arr::get($venue, 'geocodes.main.longitude');
+
+            $address = [
+                'address_1' => $addressData['address'],
+                'town_city' => $addressData['post_town'],
+                'postcode' => $addressData['postcode'],
+                'country' => $addressData['admin_region'],
+                'latitude' => $lat,
+                'longitude' => $long,
+            ];
+            return $address;
+        } catch (BindingResolutionException $e) {
+            echo 'Error building venue addrress. Caught exception: ',  $e->getMessage(), "\n";
+            return null;
+        }
+    }
+
+    public function buildAttributes($metadata)
+    {
+        try {
+            $attributes = Arr::get($metadata, 'tastes');
+            return $attributes;
+        } catch (BindingResolutionException $e) {
+            echo 'Error building venue attributes. Caught exception: ',  $e->getMessage(), "\n";
+            return null;
+        }
+    }
+
+    public function buildImages($metadata)
+    {
+        try {
+            $imageUrls = [];
+            $images = Arr::get($metadata, 'photos');
+
+            foreach ($images as $image) {
+                array_push($imageUrls, $image['prefix'] . 'original' . $image['suffix']);
+            }
+
+            return $imageUrls;
+        } catch (BindingResolutionException $e) {
+            echo 'Error building venue images. Caught exception: ',  $e->getMessage(), "\n";
+            return null;
+        }
+    }
+
+    public function buildReviews($metadata)
+    {
+        try {
+            $reviews = Arr::get($metadata, 'tips');
+            return $reviews;
+        } catch (BindingResolutionException $e) {
+            echo 'Error building venue images. Caught exception: ',  $e->getMessage(), "\n";
+            return null;
+        }
+    }
+
+    public function buildRating($metadata)
+    {
+        try {
+            $ratings = Arr::get($metadata, 'rating');
+
+            return $ratings;
+        } catch (BindingResolutionException $e) {
+            echo 'Error building venue images. Caught exception: ',  $e->getMessage(), "\n";
+            return null;
+        }
+    }
+}
